80135869fb5843988f3f300b9e2d82540f4062eb
agnieve70 <82817691+agnieve70@users.noreply.github.com>
2022-03-11 00:04:25 +0800

    added first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic25vdzA0IiwiYSI6ImNsMGw2MXg1ZzB0OXMzYnA3OWg2YXFudjcifQ.LP0Hat6q0c6GUJ2B7hRdbw";
+var map = new mapboxgl.Map({
+  container: "YOUR_CONTAINER_ELEMENT_ID",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default  App
